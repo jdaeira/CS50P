913d250ca3242b70ee154300fead8de7150fa699
jdaeira <jdaeira@gmail.com>
2023-08-07 15:06:47 -0700

    Continue Lecture 0

---
+++ Lecture0/hello.py
@@ -0,0 +1,11 @@
+# Ask user for their name
+name = input("What's your name? ")
+
+# Say hello to user
+print("Hello, " + name)
+# or
+# the comma automatically adds a space
+print("Hello,", name)
+
+
+
