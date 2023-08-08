a157352e445b99cb566d6fd2e3615f1547db14ff
jdaeira <jdaeira@gmail.com>
2023-08-07 17:39:28 -0700

    Continue Lecture 0

--- Lecture0/hello.py
+++ Lecture0/hello.py
@@ -5,7 +5,15 @@ name = input("What's your name? ")
 print("Hello, " + name)
 # or
 # the comma automatically adds a space
+
 print("Hello,", name)
 
+# or with "f" in the beginning
+print(f"Hello, {name} format")
 
+# if you use single quotes on the outside then you can use 
+# double quotes on the inside
+print('Hello, "friend"')
+# or this way
+print("Hello, \"friend\"")
 
