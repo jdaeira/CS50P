90301b6c2ba05b7d2215acad4b612917414aec10
jdaeira <jdaeira@gmail.com>
2023-08-14 17:12:54 -0700

    Starting Problem Set 0

--- Lecture0/calculator.py
+++ Lecture0/calculator.py
@@ -1,4 +1,29 @@
-x = int(input("What's x? "))
-y = int(input("What's y? "))
+# Convert strings into floats to be able to add integers
+x = float(input("What's x? "))
+y = float(input("What's y? "))
 
 print(x + y)
+
+z = round(x+y)
+
+# The z:, formats the number to put commas in the proper place
+print(f"{z:,}")
+
+# division
+
+z = round(x / y, 2)
+
+print(z)
+
+# or also by formatting
+print(f"{z:.2f}")
+
+
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
+
+def square(n):
+    return pow(n, 2)
+
+main()
