f10d118bb7d3f4673745fa65d88a57c77063717b
jdaeira <jdaeira@gmail.com>
2023-08-28 17:36:11 -0700

    Continue Problem Set 1

---
+++ Lecture1/Problem-Set-1/extensions.py
@@ -0,0 +1,25 @@
+name = input("File name: ")
+
+if(name.__contains__(".")):
+    type = name.split(".")[1]
+else:
+    type = name
+
+match type:
+    case "gif":
+        print("image/gif")
+    case "jpg":
+        print("image/jpeg")
+    case "jpeg":
+        print("image/jpeg")
+    case "png":
+        print("image/png")
+    case "pdf":
+        print("application/pdf")
+    case "txt":
+        print("text/plain")
+    case "zip":
+        print("application/zip")
+    case _:
+        print("application/octet-stream")
+
